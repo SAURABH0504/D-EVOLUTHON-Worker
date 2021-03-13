44fb0e70c8ea7a9bb7db27b8f8a86eafb9dbb58a
RitwikMehta <ritwikmehta28@yahoo.in>
2021-03-13 14:56:12 +0530

    Updated homepage.

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Service/AuthService.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,8 +11,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(title: Text('Search'),backgroundColor: Colors.green,),
 
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(child: Text('Your Profile'),
+              decoration: BoxDecoration(
+                color: Colors.greenAccent,
+              ),
+            ),
+            ListTile(
+              title:Text('Previous Transaction'),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              title:Text('Rating'),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              title:TextButton(child: Text('Logout',style: TextStyle(color: Colors.red),),onPressed: (){
+                AuthService().signOut();
+              },),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
